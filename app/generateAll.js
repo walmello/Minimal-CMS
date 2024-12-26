e5da7906f7700e84343a72e105f0ad646b133c1c
Walter Melo <41174751+walmello@users.noreply.github.com>
2024-12-26 18:53:59 -0300

    Update generateAll.js

--- app/generateAll.js
+++ app/generateAll.js
@@ -2,26 +2,26 @@ import fs from 'fs';
 //import parser from "./tree-parser/index.js";
 
 const base = "https://minimal-cms-tests.netlify.app"
-const entries = fs.readdirSync('./public').filter(entry => {
+const entries = fs.readdirSync('./src').filter(entry => {
   return !['admin', 'index.html'].includes(entry)
 })
 
 entries.forEach(entry => {
   const result = []
 
-  const subentries = fs.readdirSync(`./public/${entry}`).filter(entry => {
+  const subentries = fs.readdirSync(`./src/${entry}`).filter(entry => {
   return !['index.json'].includes(entry)
 })
   subentries.forEach(subentry => {
     const [slug , type] = subentry.split('.')
 
     if(type === 'json'){
-      const content = JSON.parse(fs.readFileSync(`./public/${entry}/${subentry}`, 'utf-8'))
+      const content = JSON.parse(fs.readFileSync(`./src/${entry}/${subentry}`, 'utf-8'))
       result.push({path: subentry, slug , ...content})
       //console.log(subentry)
     } else {
       result.push(subentry)
     }
   })
-  fs.writeFileSync(`./public/${entry}/index.json`, JSON.stringify(result, null, 2))
+  fs.writeFileSync(`./src/${entry}/index.json`, JSON.stringify(result, null, 2))
 })
